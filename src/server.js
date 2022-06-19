268649303acbe0da81cf908e55d42db26dbea25e
Adil_Askar <samask127node@gmail.com>
2022-06-19 22:54:35 +0600

    feat: implement server http

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-// import * as http from "http";
-// import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as http from "http";
+import "dotenv/config"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 
-// let port = process.env.PORT;
-// let host = process.env.HOST;
+let port = process.env.PORT;
+let host = process.env.HOSTNAME;
 
-// let server = http.createServer((req, res) => {
-//     console.log("Thanks for the request");
-//     res.writeHead(200, { "Content-Type": "text/plain" });
-//     res.end("Gravies");
-// });
+let server = http.createServer((req, res) => {
+    console.log("Thanks for the request");
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Gravies");
+});
 
-// server.listen(port, host, () => {
-//     console.log(`Server on the board ${host}:${port}`);
-// });
+server.listen(port, host, () => {
+    console.log(`Server on the board ${host}:${port}`);
+});
 
-const sau = () => console.log("Hi!");
-
-sau();
+server.on("error", (err) => {
+    console.error(`Got error: ${err.message}`);
+    // Check if retry is needed
+    // if (req.reusedSocket && err.code === 'ECONNRESET') {
+    //   retriableRequest();
+    // }
+});
